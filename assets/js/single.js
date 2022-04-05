61737db5738563a695b2857d2718be6273aec090
akeva22 <melchorak@gmail.com>
2022-04-05 11:12:27 -0700

    ask bcs 2.2

--- assets/js/single.js
+++ assets/js/single.js
@@ -7,33 +7,31 @@ var getRepoName = function () {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
     getRepoIssues(repoName)
-    repoNameEl.textContent = repoName;
+    repoNameEl.textContent = repoName
 
 }
 
-var getRepoIssues = function(repo) {
-    // format the github api url
+
+var getRepoIssues = function (repo) {
+    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-  
-    // make a get request to url
-    fetch(apiUrl).then(function(response) {
-      // request was successful
-      if (response.ok) {
-        response.json().then(function(data) {
-          displayIssues(data);
-  
-          // check if api has paginated issues
-          if (response.headers.get("Link")) {
+    fetch(apiUrl).then(function (response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                // pass response data to dom function
+                displayIssues(data);
+            });
+        }
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
             displayWarning(repo);
-          }
-        });
-      }
-      else {
-        console.log(response);
-        alert("There was a problem with your request!");
-      }
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
     });
-  };
+};
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
